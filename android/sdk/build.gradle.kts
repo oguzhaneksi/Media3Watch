21b2a7515bdb189979f3009ffe6fbac5c407ebf1
oguzhaneksi <oguzhan.eksi97@gmail.com>
2026-02-24 23:09:11 +0400

    feat: Implement Maven Central publishing for the Android SDK, including CI/CD workflow and documentation.

--- android/sdk/build.gradle.kts
+++ android/sdk/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.jetbrains.dokka)
 }
 
 android {
@@ -47,4 +49,44 @@ dependencies {
     testImplementation(libs.okhttp.mockwebserver)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.oguzhaneksi",
+        artifactId = "media3watch-sdk",
+        version = "1.0.0-alpha01"
+    )
+
+    pom {
+        name.set("Media3Watch SDK")
+        description.set("Android SDK for tracking Media3 ExoPlayer QOE metrics easily.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/oguzhaneksi/Media3Watch")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("oguzhaneksi")
+                name.set("Oğuzhan Ekşi")
+                url.set("https://github.com/oguzhaneksi")
+            }
+        }
+        scm {
+            url.set("https://github.com/oguzhaneksi/Media3Watch")
+            connection.set("scm:git:git://github.com/oguzhaneksi/Media3Watch.git")
+            developerConnection.set("scm:git:ssh://git@github.com/oguzhaneksi/Media3Watch.git")
+        }
+    }
+
+    // Configure publishing to Maven Central Portal
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
